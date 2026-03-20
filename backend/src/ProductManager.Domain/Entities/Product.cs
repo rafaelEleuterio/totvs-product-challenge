4f57a2fd44808f32a49087eb59f511f5c649efda
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2026-03-19 22:21:30 -0300

    Add: Configuration of the table created automatically

--- backend/src/ProductManager.Domain/Entities/Product.cs
+++ backend/src/ProductManager.Domain/Entities/Product.cs
@@ -1,8 +1,18 @@
-﻿namespace ProductManager.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductManager.Domain.Entities;
 public class Product
 {
+    [Key]
     public Guid Id { get; set; }
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Price { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
     public int Stock { get; set; }
 }
